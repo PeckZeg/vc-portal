5ad87459cefb71e369b3ab34abe46710d407a7df
PeckZeg <peck.zeg@gmail.com>
2018-06-15 18:14:21 +0800

    :sparkles: update

---
+++ build/webpack.build.js
@@ -0,0 +1,34 @@
+const merge = require('webpack-merge');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const baseConfig = require('./webpack.site.base');
+
+const { resolvePath } = require('./utils');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+
+    entry: './src/index.jsx',
+
+    output: {
+        filename: 'main.js',
+        path: resolvePath('dist')
+    },
+
+    externals: {
+        vue: 'Vue'
+    },
+
+    // optimization: {
+    //     minimize: false
+    // },
+
+    plugins: [
+      new CleanWebpackPlugin(['dist'], {
+        root: process.cwd()
+      })
+    ]
+});
